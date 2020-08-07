fea605596d098a4ad5f6d4a83825a14ae10bf871
Turkay Tunc <turkaytunc00@gmail.com>
2020-08-07 10:05:39 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import { Counter } from "./components/Counter";
 import { CartContext } from "./CartContext";
 
+const fakeData = {
+  firstName: "",
+  lastName: "",
+  age: null,
+};
+
 function App() {
+  const [data, setData] = useState(fakeData);
   return (
-    <CartContext.Provider>
-      <div className="App"></div>
+    <CartContext.Provider value={{ data, setData }}>
+      <div className="App">
+        <Counter></Counter>
+      </div>
     </CartContext.Provider>
   );
 }
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
+import { CartContext } from "../CartContext";
 
 export const Counter = () => {
+  const cart = useContext(CartContext);
+
   return (
     <div>
-      <button>Increment</button>
-      <button>Decrement</button>
-      <button>Reset</button>
+      <input
+        type="text"
+        value={cart.data.firstName}
+        placeholder="FirstName"
+        onChange={(e) =>
+          cart.setData({ ...cart.data, firstName: e.target.value })
+        }
+      />
     </div>
   );
 };
