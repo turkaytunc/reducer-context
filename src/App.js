5f645d4c92603fc1b2897193d9f9849fa1ddb7c3
Turkay Tunc <turkaytunc00@gmail.com>
2020-08-07 15:18:21 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useReducer } from "react";
 import "./App.css";
 import { Counter } from "./components/Counter";
 import { CartContext } from "./CartContext";
@@ -9,10 +9,23 @@ const fakeData = {
   age: null,
 };
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "firstName":
+      return { ...state, firstName: action.value };
+    case "lastName":
+      return { ...state, lastName: action.value };
+    case "age":
+      return { ...state, age: action.value };
+    default:
+      return state;
+  }
+};
+
 function App() {
-  const [data, setData] = useState(fakeData);
+  const [data, dispatch] = useReducer(reducer, fakeData);
   return (
-    <CartContext.Provider value={{ data, setData }}>
+    <CartContext.Provider value={{ data, dispatch }}>
       <div className="App">
         <Counter></Counter>
       </div>
